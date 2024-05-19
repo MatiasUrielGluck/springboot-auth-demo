89c043ee9022fc528c139009125b946223a42573
Matias Gluck <matiasugluck@gmail.com>
2024-05-19 17:37:19 -0300

    feat: app init with user entity

---
+++ src/main/java/com/example/authdemo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.authdemo.repositories;
+
+import com.example.authdemo.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
